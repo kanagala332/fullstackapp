44e5bd6ac3ab29dea9dca73201ae7fb904b705ac
kanagala332 <Kanagalaharshavardhan@gmail.com>
2025-09-21 18:22:31 +0530

    Update AppController.java

--- crud_backend/crud_backend-main/src/main/java/com/klu/AppController.java
+++ crud_backend/crud_backend-main/src/main/java/com/klu/AppController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 public class AppController {
 	@Autowired
-	Service s;
+	ProductService s;
 
 	@PostMapping("/insert")
 	public String insert(@RequestBody Product p) {
